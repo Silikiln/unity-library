f9be4402b7f101eb99d60d34797d2392a95ad5d6
Austin Beeler <abeeler1@uncc.edu>
2017-01-28 11:33:58 -0500

    Add ButtonUp and ButtonDown helper properties to GamepadInput

--- GamepadInput.cs
+++ GamepadInput.cs
@@ -42,33 +42,53 @@ public class GamepadInput {
 	// Buttons
 	public string A_Button { private set; get; }
 	public bool A { get { return Input.GetButton (A_Button); } }
+   public bool ADown { get { return Input.GetButtonDown (A_Button); } }
+   public bool AUp { get { return Input.GetButtonUp (A_Button); } }
 
 	public string B_Button { private set; get; }
 	public bool B { get { return Input.GetButton (B_Button); } }
+   public bool BDown { get { return Input.GetButtonDown (B_Button); } }
+   public bool BUp { get { return Input.GetButtonUp (B_Button); } }
 
 	public string X_Button { private set; get; }
 	public bool X { get { return Input.GetButton (X_Button); } }
+	public bool XDown { get { return Input.GetButtonDown (X_Button); } }
+	public bool XUp { get { return Input.GetButtonUp (X_Button); } }
 
 	public string Y_Button { private set; get; }
 	public bool Y { get { return Input.GetButton (Y_Button); } }
+	public bool YDown { get { return Input.GetButtonDown (Y_Button); } }
+	public bool YUp { get { return Input.GetButtonUp (Y_Button); } }
 
 	public string RightBumper_Button { private set; get; }
 	public bool RB { get { return Input.GetButton (RightBumper_Button); } }
+	public bool RBDown { get { return Input.GetButtonDown (RightBumper_Button); } }
+	public bool RBUp { get { return Input.GetButtonUp (RightBumper_Button); } }
 
 	public string LeftBumper_Button { private set; get; }
 	public bool LB { get { return Input.GetButton (LeftBumper_Button); } }
+	public bool LBDown { get { return Input.GetButtonDown (LeftBumper_Button); } }
+	public bool LBUp { get { return Input.GetButtonUp (LeftBumper_Button); } }
 
 	public string Back_Button { private set; get; }
 	public bool Back { get { return Input.GetButton (Back_Button); } }
+	public bool BackDown { get { return Input.GetButtonDown (Back_Button); } }
+	public bool BackUp { get { return Input.GetButtonUp (Back_Button); } }
 
 	public string Start_Button { private set; get; }
 	public bool Start { get { return Input.GetButton (Start_Button); } }
+	public bool StartDown { get { return Input.GetButtonDown (Start_Button); } }
+	public bool StartUp { get { return Input.GetButtonUp (Start_Button); } }
 
 	public string LeftStick_Button { private set; get; }
 	public bool LS { get { return Input.GetButton (LeftStick_Button); } }
+	public bool LSDown { get { return Input.GetButtonDown (LeftStick_Button); } }
+	public bool LSUp { get { return Input.GetButtonUp (LeftStick_Button); } }
 
 	public string RightStick_Button { private set; get; }
 	public bool RS { get { return Input.GetButton (RightStick_Button); } }
+	public bool RSDown { get { return Input.GetButtonDown (RightStick_Button); } }
+	public bool RSUp { get { return Input.GetButtonUp (RightStick_Button); } }
 
 	public bool DPadLeft { get { return DPad_X < -DPadDeadzone; } }
 	public bool DPadRight { get { return DPad_X > DPadDeadzone; } }
